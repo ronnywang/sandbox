b699bd8c02529ff8c1c1d7e4a63e898909fd3bab
Ronny Wang <ronnywang@gmail.com>
2016-03-21 16:33:40 +0800

    Update yi.js

--- yi.js
+++ yi.js
@@ -1,6 +1,6 @@
 tr=document.getElementsByClassName('stats-chart-gviz')[0].getElementsByTagName('svg')[0].nextSibling.getElementsByTagName('tr');
 ret="";
-for(var i=1;i<tr.length;i++){tds=tr[i].getElementsByTagName('td');ret+=(tds[0].innerText+","+tds[1].innerText+"\n");}
+for(var i=1;i<tr.length;i++){tds=tr[i].getElementsByTagName('td');ret+=(tds[0].innerText+","+tds[1].innerText+"\r\n");}
 var saveData = (function () {
     var a = document.createElement("a");
     document.body.appendChild(a);
