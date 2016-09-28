cb6ee54624274ad2ccd88cee8c31896149ce687a
Ronny Wang <ronnywang@gmail.com>
2016-09-28 10:11:41 +0800

    Update yi.js

--- yi.js
+++ yi.js
@@ -1,10 +1,16 @@
 tr=document.getElementsByClassName('stats-chart-gviz')[0].getElementsByTagName('svg')[0].nextSibling.getElementsByTagName('tr');
+if ('undefined' == typeof(yi_sep)) {
+    yi_sep = ',';
+}
+if ('undefined' == typeof(yi_type)) {
+    yi_type = 'csv';
+}
 ret="";
 var prev_value = 0;
 for(var i = 1; i < tr.length; i ++){
     tds = tr[i].getElementsByTagName('td');
     v = parseInt(tds[1].innerText.replace(/,/g, ''));
-    ret += (tds[0].innerText + "," + v + "," + parseInt(v - prev_value) + "\r\n");
+    ret += (tds[0].innerText + yi_sep + v + yi_sep + parseInt(v - prev_value) + "\r\n");
     prev_value = v;
 }
 var saveData = (function () {
@@ -21,4 +27,4 @@ var saveData = (function () {
     };
 }());
 
-saveData(ret, "insight.csv");
+saveData(ret, "insight." + yi_type);
